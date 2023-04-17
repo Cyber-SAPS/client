b5b8b1d42990e63ae2b918712aa336350d8fd7f8
Daryl Evans <csaps@Daryls-MacBook-Pro.local>
2023-04-16 22:39:54 -0400

    Updated app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import { Layout } from './components/ui/layout'
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter,Routes, Route } from "react-router-dom"
 import Home from './pages/home';
 import Faq from './pages/faq';
 import Contact from './pages/contact';
@@ -11,7 +11,9 @@ import EventCalendar from './pages/calendar';
 function App(){
 
   return (
+    <BrowserRouter basename='client'>
     <Layout>
+      
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="faq" element={ <Faq/> } />
@@ -19,6 +21,7 @@ function App(){
         <Route path="events" element={ <EventCalendar/> } />
       </Routes>
     </Layout>
+    </BrowserRouter>
   );
 }
 
